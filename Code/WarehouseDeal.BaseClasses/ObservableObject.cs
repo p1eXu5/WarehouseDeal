5f0f915f0c8a928700f46ee3bfc994687da1f18f
p1eXu5 <v1adp1exu5@gmail.com>
2018-03-29 16:37:35 +0300

    итьить

---
+++ Code/WarehouseDeal.BaseClasses/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDeal.BaseClasses
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            handler?.Invoke (this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Code/WarehouseDeal.BaseClasses/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDeal.BaseClasses
+{
+    public abstract class ViewModel : ObservableObject, IDataErrorInfo
+    {
+        public string this[string columnName] => OnValidate (columnName);
+
+        public string Error => throw new NotImplementedException ();
+
+        protected virtual string OnValidate (string propertyName)
+        {
+            var context = new ValidationContext (this)
+                            {
+                                MemberName = propertyName
+                            };
+
+            var results = new Collection<ValidationResult> ();
+            var isValid = Validator.TryValidateObject (this, context, results, true);
+
+            if (!isValid) {
+
+                ValidationResult result = results.SingleOrDefault(p => p.MemberNames.Any(memberName => memberName == propertyName));
+
+                return result?.ErrorMessage;
+            }
+
+            return null;
+
+        }
+    }
+}
