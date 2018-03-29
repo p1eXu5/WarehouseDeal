365c7e4f4d6892baa4d90b889afe05be66c665df
p1eXu5 <v1adp1exu5@gmail.com>
2018-03-29 16:38:05 +0300

    прпррпрп

---
+++ Tests/WarehouseDeal.BaseClasses.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("WarehouseDeal.BaseClasses.Tests")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("WarehouseDeal.BaseClasses.Tests")]
+[assembly: AssemblyCopyright ("Copyright ©  2018")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+
+[assembly: Guid ("0209d754-f0a3-4db4-bb58-aaabf44a6218")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
---
+++ Tests/WarehouseDeal.BaseClasses.Tests/UnitTests/ViewModelTests.cs
@@ -0,0 +1,54 @@
+﻿namespace WarehouseDeal.BaseClasses.Tests.UnitTests
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ViewModelTests
+    {
+        [TestMethod]
+        public void ViewModel_IsAbstractClass()
+        {
+            Type t = typeof(ViewModel);
+
+            Assert.IsTrue (t.IsAbstract);
+        }
+
+        [TestMethod]
+        public void IsDataErrorInfo()
+        {
+            Assert.IsTrue (typeof(IDataErrorInfo).IsAssignableFrom (typeof (ViewModel)));
+        }
+
+        [TestMethod]
+        public void IndexerPropertyValidatesPropertyNameWithInvalidValue()
+        {
+            var obj = new StubViewModel ();
+
+            Assert.IsNotNull (obj["RequiredProperty"]);
+        }
+
+        [TestMethod]
+        public void IndexerPropertyValidatesPropertyNameWithValidValue ()
+        {
+            var obj = new StubViewModel
+            {
+                RequiredProperty = "Some property"
+            };
+
+            Assert.IsNull (obj["RequiredProperty"]);
+        }
+
+        class StubViewModel : ViewModel
+        {
+            [Required]
+            public string RequiredProperty { get; set; }
+
+            [Required]
+            [StringLength(32, MinimumLength = 4)]
+            public string SomeProperty { get; set; }
+        }
+    }
+}
